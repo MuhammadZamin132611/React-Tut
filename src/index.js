be115c3ec7f564e6c303e1f5a3aaa9aaa9bec4c5
Zamin <Muhammad.Zamin@pratian.com>
2024-03-08 12:40:20 +0530

    add class component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Users from './Users'
-
+// import Users from './Users'
+import About from './About';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Users />
+    <About />
   </React.StrictMode>
 );
 
